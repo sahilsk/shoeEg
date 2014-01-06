e3f4130c7fab04bfe49acf3324a97a5642f56b1f
sahilsk <sonu.k.meena@apyl.com>
2014-01-06 16:04:37 +0530

    changes

--- client.js
+++ client.js
@@ -4,6 +4,43 @@ var reconnect = require("reconnect/shoe");
 var randomColor = require('random-color')
 
 
+var r = reconnect( function(stream){
+        var d = dnode();
+        d.pipe(stream).pipe(d);
+        d.on("remote",   handleLink);
+    
+    
+}).connect("/stream");
+
+
+var handleLink = functioin(remote){
+    
+    setInterval(function(){  
+        console.log("calling remote methods..");
+        
+        var dateDiv = document.getElementById('date');
+        var timeDiv = document.getElementById('time');
+        
+        var oldDateTime ={ 'date': dateDiv.textContent, 'time': timeDiv.textContent };
+                          
+        remote.currentTime( JSON.stringify(oldDateTime), function(newDateTime){
+            var newDateTimeObj = JSON.parse( newDateTime);
+            console.log( "new time: " , newDateTimeObj.time);
+            dateDiv.textContent = newDateTimeObj.date;
+            
+            timeDiv.textContent = newDateTimeObj.time;
+            timeDiv.style.color = randomColor();
+        });    
+    }, 1000);    
+}
+
+
+
+/***
+Without reconnect 
+
+***********
+
 var stream  = shoe("/stream");
 var d = dnode();
 d.pipe(stream).pipe(d);
@@ -29,21 +66,4 @@ d.on('remote', function(remote){
 });
 
 
-/*
-reconnect(function(stream){
-    console.log("trying connecting to dnode...");
-    var d = dnode();
- //   setInterval(function(){
-        d.on('remote', function(remote){
-            console.log("calling remote methods..");
-            remote.currentTime( (new Date()).toDateString(), function(newDate){
-                resultDiv.textContent = newDate;
-            });
-        });
-    
-//    }, 3000);    
-
-    d.pipe(stream).pipe(d);
-
-}).connect('/stream');
 */
