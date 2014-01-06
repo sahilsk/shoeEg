7dc1f974d24c681a319f27f541c68cbcdb10ec15
sahilsk <sonu.k.meena@apyl.com>
2014-01-06 11:39:04 +0530

    changes

--- client.js
+++ client.js
@@ -5,33 +5,25 @@ var reconnect = require("reconnect/shoe");
 var resultDiv = document.getElementById('result');
 
 var stream  = shoe("/stream");
-
 var d = dnode();
-d.on('remote', function(remote){
-    console.log("calling remote methods..");
-    remote.currentTime( (new Date()).toDateString(), function(newDate){
-        console.log( "new date: " , newDate);
-        var resultDiv = document.getElementById('result');
-
-        resultDiv.textContent = newDate;
-    });
-});
-
 d.pipe(stream).pipe(d);
 
 
 
-/*
 setInterval(function(){
     d.on('remote', function(remote){
         console.log("calling remote methods..");
         remote.currentTime( (new Date()).toDateString(), function(newDate){
-            resultDiv.textContent += "<br/>" + newDate;
+            console.log( "new date: " , newDate);
+            var resultDiv = document.getElementById('result');
+            resultDiv.textContent = newDate;
         });
     });
 
 }, 3000);
 
+
+/*
 
 
 reconnect(function(stream){
